a00838033209dfa228b67c9e59f3fe58eafbccf7
Brian Pfretzschner <brian.pfretzschner@gmail.com>
2025-11-21 20:07:09 +0100

    Cleanup OptaPlanner files for the moment

--- backend/src/main/java/de/brianp/config/SecurityConfig.java
+++ backend/src/main/java/de/brianp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,16 +13,20 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authz -> authz.requestMatchers("/", "/error", "/webjars/**").permitAll()
-                .requestMatchers("/login-success.html", "/login-failure.html", "/calendar.html").permitAll()
-                .requestMatchers("/api/auth/**").permitAll().requestMatchers("/api/calendar/status").permitAll()
-
-                .anyRequest().authenticated())
+        return http.authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
                 .oauth2Login(oauth2 -> oauth2.loginPage("/oauth2/authorization/google")
-                        .defaultSuccessUrl("/login-success.html", true).failureUrl("/login-failure.html"))
-                .logout(logout -> logout.logoutSuccessUrl("/").invalidateHttpSession(true).clearAuthentication(true))
-                .csrf(csrf -> csrf.disable()).headers(headers -> headers.frameOptions().sameOrigin());
-
-        return http.build();
+                        .defaultSuccessUrl("/login-success.html", true)
+                        .failureUrl("/login-failure.html")
+                )
+                .logout(logout -> logout.logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions().sameOrigin())
+                .build();
     }
 }
